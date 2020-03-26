05d8ef4a851ca47730a726ad18f4ab98491804dc
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-03-26 18:09:33 +0100

    Added script to build distribution folder

---
+++ Build-Dist.ps1
@@ -0,0 +1,17 @@
+# Build program using Maven
+$mavenProcess = (Start-Process "mvn" -NoNewWindow -PassThru -Wait -ArgumentList @("clean", "package"))
+if ($mavenProcess.ExitCode -ne 0) {
+    Write-Warning "Failed to build sources. Check Maven output."
+    exit
+}
+
+# Create dist folder and copy relevant files
+New-Item ./target/dist -ItemType Directory | out-null
+New-Item ./target/dist/libs -ItemType Directory | out-null
+Copy-Item ./target/mailbackup-*.jar ./target/dist/
+Copy-Item ./src/main/bash/*.sh ./target/dist/
+Copy-Item ./src/main/powershell/*.ps1 ./target/dist/
+Copy-Item ~/.m2/repository/com/sun/mail/javax.mail/1.6.2/*.jar ./target/dist/libs/
+Copy-Item ~/.m2/repository/commons-cli/commons-cli/1.4/*.jar ./target/dist/libs/
+Copy-Item ~/.m2/repository/javax/activation/activation/1.1/*.jar ./target/dist/libs/
+Write-Host "Distribution has been written to ./target/dist"
