c8cded8ac2464e9d275d076e0bc1a508a84d3635
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-01-03 23:26:50 +0100

    Improved build script for ZIP distribution

--- Build-Dist.ps1
+++ Build-Dist.ps1
@@ -14,4 +14,11 @@ Copy-Item ./src/main/powershell/*.ps1 ./target/dist/
 Copy-Item ~/.m2/repository/com/sun/mail/javax.mail/1.6.2/*.jar ./target/dist/libs/
 Copy-Item ~/.m2/repository/commons-cli/commons-cli/1.4/*.jar ./target/dist/libs/
 Copy-Item ~/.m2/repository/javax/activation/activation/1.1/*.jar ./target/dist/libs/
-Write-Host "Distribution has been written to ./target/dist"
+Copy-Item ./LICENSE.md ./target/dist/
+
+# Create ZIP archive
+$jarFileName = Get-ChildItem -Path ./target/ -Filter mailbackup-*.jar -Name
+$patternMatches = $jarFileName | Select-String -Pattern 'mailbackup-(.+?).jar'
+$version = $patternMatches.matches.groups[1]
+Compress-Archive -Path ./target/dist/* -DestinationPath "./target/v$version.zip"
+Write-Host "Distribution has been written to ./target/v$version.zip"
